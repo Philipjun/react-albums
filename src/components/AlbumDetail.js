bbe942a94a34d4b043e4cf18e8780c0a9ebbdd52
Philip Jun <inbox@philipjun.com>
2017-03-02 21:10:46 -0500

    refactor album detail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class AlbumDetail extends Component {
+  render() {
+    return (
+
+    );
+  }
+
+
+}
